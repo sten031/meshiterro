b9aabf508422f63a762c638e193c39e0bfcf6a12
shingo tanaka <stenstenktensten@gmail.com>
2020-04-08 14:05:23 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'ae2c753b0a59f13f5c150e5f9a85cfe69f2240eab225583f2c348398d2133659e0257a22d428546e332b8215fb10260fcbb6f29168d4a6bfabbe9cbbf90ec373'
